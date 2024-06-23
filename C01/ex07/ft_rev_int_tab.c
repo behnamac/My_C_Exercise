35a043baa7476a7b65c0e1122f58be17cacb1c7b
Behnam Sepehri <bsepehri@f0r2s3.codam.nl>
2024-06-23 17:10:02 +0200

    add quick sort

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,35 +1,26 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void f_print(char c)
-{
-	write(1,&c,1);
-}
-void f_converter(int a)
-{
-	char c;
-
-	c = '0'+ a;
-	f_print(c);
-}
 void	ft_rev_int_tab(int *tab, int size)
 {
-	unsigned int i;
+	int start = 0;
+	int end = size - 1;
+	int temp;
 
-	i=size-1;
-	while(i>0&& *tab!='\0')
+	while (start <= end && tab!=0)
 	{
-		f_converter(tab[i]);
-		tab++;
-		i--;
+		temp=tab[start];
+		tab[start]=tab[end];
+		tab[end]=temp;
+		start++;
+		end--;
 	}
-	tab[size]='\0';
-	
 }
 // int main()
 // {
-// 	int arr[]={1,5,7,9};
+// 	int arr[] = {1,5,7,9};
 // 	ft_rev_int_tab(arr,4);
+// 	printf("%d\n",*arr);
 // 	return (0);
 // }
 

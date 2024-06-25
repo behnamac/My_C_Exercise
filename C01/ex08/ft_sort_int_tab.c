ae2e024fdd1eb06f50c857d81f952f15d48d3a05
Behnam Sepehri <bsepehri@f0r1s5.codam.nl>
2024-06-25 12:25:42 +0200

    add new exes

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,74 +1,56 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void f_print(char c)
-{
-	write(1,&c,1);
-}
-
-void f_converter(int a)
-{
-	char c;
-	c = '0' + a;
-	f_print(c);
-}
-
-void swap(int* a, int* b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // pivot
-    int i = (low - 1); // Index of smaller element
-    int j = low;
-
-    while (j <= high - 1) {
-        if (arr[j] < pivot) {
-            i++; // increment index of smaller element
-            swap(&arr[i], &arr[j]);
-        }
-        j++;
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
-}
-
-void quickSortRecursive(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-
-        quickSortRecursive(arr, low, pi - 1);
-        quickSortRecursive(arr, pi + 1, high);
-    }
-}
 
 void ft_sort_int_tab(int *tab, int size)
 {
-    if (size > 1) {
-        quickSortRecursive(tab, 0, size - 1);
-    }
+	int i = 0;
+	int spwapped = 1;
+	int temp;
+
+	while(spwapped)
+	{
+		i=0;
+		spwapped = 0;
+		while(i<size-1)
+		{
+			if(tab[i]>tab[i+1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i+1];
+				tab[i+1] = temp;
+				spwapped = 1;
+			}
+			i++;
+		}
+		size--;
+	}
+	
 }
 
-void printArray(int arr[], int size) {
+void printArray(int *arr, int size) {
     int i = 0;
     while (i < size) {
-        f_converter(arr[i]);
+        printf("%d ", arr[i]);
         i++;
     }
-    f_print('\n');
+    printf("\n");
 }
-// int main() {
-//     int arr[] = {10, 7, 8, 9, 1, 5};
-//     int n = sizeof(arr) / sizeof(arr[0]);
-//     printf("Unsorted array: \n");
-//     printArray(arr, n);
-//     ft_sort_int_tab(arr, n);
-//     printf("Sorted array: \n");
-//     printArray(arr, n);
-//     return 0;
-// }
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    ft_sort_int_tab(arr, n);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
+
+    return 0;
+}
 
 
 
